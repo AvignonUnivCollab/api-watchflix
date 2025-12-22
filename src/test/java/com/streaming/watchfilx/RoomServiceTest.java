20abab6eab90f43027d2a6952b9f524289999571
Daliaben23 <bensaiddalia23@gmail.com>
2025-12-22 20:49:44 +0100

    roomservicetest

---
+++ src/test/java/com/streaming/watchfilx/RoomServiceTest.java
@@ -0,0 +1,14 @@
+package com.streaming.watchflix;
+
+import com.streaming.watchflix.services.RoomService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomServiceTest {
+
+    @Test
+    void contextLoads() {
+        // test vide pour l’instant
+    }
+}
