27e895215120dae56a53e3152b344a659411b145
Daliaben23 <bensaiddalia23@gmail.com>
2025-12-23 20:20:45 +0100

    MessageServiceTest

---
+++ src/test/java/com/streaming/watchfilx/MessageServiceTest.java
@@ -0,0 +1,13 @@
+package com.streaming.watchfilx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceTest {
+
+    @Test
+    void contextLoads() {
+        // Test simple : vérifie que le contexte Spring démarre
+    }
+}
