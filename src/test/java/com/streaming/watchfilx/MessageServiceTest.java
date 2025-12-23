26206aa0fa2c8f975d550bfdb17688b294c08bfa
Daliaben23 <bensaiddalia23@gmail.com>
2025-12-23 20:38:30 +0100

    user test

--- src/test/java/com/streaming/watchfilx/MessageServiceTest.java
+++ src/test/java/com/streaming/watchfilx/MessageServiceTest.java
@@ -1,13 +1,45 @@
 package com.streaming.watchfilx;
 
+import com.streaming.watchfilx.models.Role;
+import com.streaming.watchfilx.models.User;
+import com.streaming.watchfilx.repositories.RoomRepository;
+import com.streaming.watchfilx.repositories.UserRepository;
+import com.streaming.watchfilx.services.MessageService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class MessageServiceTest {
 
+    @Autowired
+    private MessageService messageService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
     @Test
     void contextLoads() {
-        // Test simple : vérifie que le contexte Spring démarre
+        // Vérifie que le contexte Spring démarre correctement
+    }
+
+    @Test
+    void createUser_shouldSaveUser() {
+
+        User user = new User();
+        user.setEmail("testuser@mail.com");
+        user.setNom("Test");
+        user.setPrenom("User");
+        user.setPassword("password");
+        user.setRole(Role.USER);
+
+        User savedUser = userRepository.save(user);
+
+        assertNotNull(savedUser.getId());
     }
 }
