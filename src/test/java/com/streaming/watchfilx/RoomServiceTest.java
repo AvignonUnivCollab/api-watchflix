8f7aca1ec13dcfebe1c84f6480deef96825df855
lina8552 <lina.ould-amer@alumni.univ-avignon.fr>
2026-01-02 19:16:40 +0100

    Ajout des tests métier pour Video et Playlist

--- src/test/java/com/streaming/watchfilx/RoomServiceTest.java
+++ src/test/java/com/streaming/watchfilx/RoomServiceTest.java
@@ -11,11 +11,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class RoomServiceTest {
 
